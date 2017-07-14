af37a8a6854daae682b4916c4879a1eea8e1fe47
vethastanley <vethastanley@gmail.com>
2017-07-14 10:23:56 +0530

    start stop script

--- parking-pi-agent-startup.sh
+++ parking-pi-agent-startup.sh
@@ -6,6 +6,7 @@ SUBSCRIBER_KEY=sub-c-313d4a34-2ef7-11e6-b700-0619f8945a4f
 API_KEY=b082de9b-aa0d-49f6-8673-088384c5931f
 ENDPOINT=/gateway/ParkingManagementAPI/parkingmgmt/locations
 
-CLASSPATH=".:/lib/parking-agent-1.0-SNAPSHOT.jar:/lib/async-http-client-1.7.17.jar;/lib/bcprov-jdk16-1.46.jar;/lib/guava-15.0.jar;/lib/json-20090211.jar;/lib/json-simple-1.1.jar;/lib/netty-3.6.6.Final.jar;/lib/pubnub-3.7.10.jar;/lib/slf4j-api-1.7.2.jar"
+CLASSPATH=".:/var/lib/parkingpi-agent/lib/parking-agent-1.0-SNAPSHOT.jar:/var/lib/parkingpi-agent/lib/async-http-client-1.7.17.jar;/var/lib/parkingpi-agent/lib/bcprov-jdk16-1.46.jar;/var/lib/parkingpi-agent/lib/guava-15.0.jar;/var/lib/parkingpi-agent/lib/json-20090211.jar;/var/lib/parkingpi-agent/lib/json-simple-1.1.jar;/var/lib/parkingpi-agent/lib/netty-3.6.6.Final.jar;/var/lib/parkingpi-agent/lib/pubnub-3.7.10.jar;/var/lib/parkingpi-agent/lib/slf4j-api-1.7.2.jar"
 
-java -agentlib:jdwp=transport=dt_shmem,server=y,suspend=n -Xms128m -Xmx384m -Xnoclassgc -classpath ${CLASSPATH} com.parking.agent.ParkingAgentStarter ${HOST} ${PORT} ${PUBLISH_KEY} ${SUBSCRIBER_KEY} ${API_KEY} ${ENDPOINT}
+sudo java -classpath ${CLASSPATH} com.parking.agent.ParkingAgentStarter ${HOST} ${PORT} ${PUBLISH_KEY} ${SUBSCRIBER_KEY} ${API_KEY} ${ENDPOINT} &>> /var/log/parkingagent.log &
+sudo echo $! > /var/lib/parkingpi-agent/piprocess.pid &
---
+++ parking-pi-agent-stop.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo kill -9 `cat /var/lib/parkingpi-agent/piprocess.pid` &
